aeaabcc97975052d42ed44a0d481172df878bbc6
Keetta <nicholasfelixloko2@gmail.com>
2025-08-28 20:38:54 -0300

    Desenvolvi Supplier Cart

--- src/js/index.js
+++ src/js/index.js
@@ -22,6 +22,59 @@ function renderProductsTable() {
   }
 }
 
+function renderSupplierCart() {
+  const paymentCart = document.getElementById('payment-cart');
+  if (!paymentCart) return;
+
+  paymentCart.innerHTML = '';
+
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement('div');
+    div.classList = 'card payment-table'
+
+    const tbody = document.createElement('tbody');
+    tbody.innerHTML = '';
+
+    for (let i = 1; i <= 5; i++) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td class="description">
+          <div>
+            <span>Pneu ${i}</span>
+            <span class="badge">PCR</span>
+          </div>
+        </td>
+        <td class="quantity">
+          <input class="t-right" type="text" id="item-${i}" name="quantity" oninput="onChangeQuantity(${i})">
+        </td>
+      `;
+      tbody.appendChild(tr);
+    }
+
+    div.innerHTML = `
+        <div class="supplier-info">
+          <span class="nome">LL</span>
+          <div>
+            <span>2500</span>
+            <span><i class="bi bi-box-seam"></i> 2</span>
+          </div>
+        </div>
+        <div class="scroll-table">
+        <table class="supplier-table">
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Quantidade</th>
+              </tr>
+            </thead>
+            ${tbody.innerHTML}
+        </table>
+        </div>
+    `;
+    paymentCart.appendChild(div);
+  }
+}
+
 function onChangeQuantity(i) {
   const item = document.getElementById(`item-${i}`);
   let value = item.value.replace(/[^0-9]/g, "") ?? 0;
@@ -41,3 +94,4 @@ function formatCurrency(value, locale = 'en-US', currency = 'USD') {
 }
 
 renderProductsTable();
+renderSupplierCart()
