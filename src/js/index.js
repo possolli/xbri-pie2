9174e8e3e6c8dedb4166b10b9132931d590f2886
possolli <jose.passolli@gmail.com>
2025-09-04 20:23:56 -0300

    ajustado para index.js não ser modulo

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
-import { get } from "./api.js";
+const API_URL = "http://localhost:8000";
+
+async function get() {
+  const response = await fetch(`${API_URL}/products`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  });
+
+  if (!response.ok) {
+    throw new Error("HTTP Error: " + response.status);
+  }
+
+  const data = await response.json();
+  return data;
+}
 
 let products = [];
 
