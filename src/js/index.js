339fb5663815ffb4d01aca73f5b95d0ac8dea782
jho <jho.martiori@gmail.com>
2025-10-06 20:56:40 -0300

    feat(cart): include functionality to add products on the cart

--- src/js/index.js
+++ src/js/index.js
@@ -21,7 +21,6 @@ function init() {
     products = data;
     console.log(products);
     renderProductsTable();
-    renderSupplierCart();
   });
 }
 
@@ -45,7 +44,7 @@ function renderProductsTable() {
           type="text"
           id="item-${product.id}"
           name="quantity"
-          oninput="onChangeQuantity(${product.id})"
+          oninput="onChangeQuantity(${product.id}, 'item')"
         >
       </td>
       <td class="t-right total40hc">${formatNumber(product.capacity_40hc)}</td>
@@ -61,34 +60,44 @@ function renderSupplierCart() {
 
   paymentCart.innerHTML = '';
 
-  for (let i = 1; i <= 5; i++) {
+  const groupedBySupplier = cart.reduce((acc, item) => {
+    if (!acc[item.supplier]) {
+      acc[item.supplier] = [];
+    }
+    acc[item.supplier].push(item);
+    return acc;
+  }, {});
+
+  for (const supplier in groupedBySupplier) {
+    supplier.totalQuantity = groupedBySupplier[supplier].reduce((sum, item) => sum + item.quantity, 0);
+
     const div = document.createElement('div');
-    div.classList = 'card payment-table'
+    div.classList = 'payment-table'
 
     const tbody = document.createElement('tbody');
     tbody.innerHTML = '';
 
-    for (let i = 1; i <= 5; i++) {
+    groupedBySupplier[supplier].forEach(item => {
       const tr = document.createElement('tr');
       tr.innerHTML = `
         <td class="description">
           <div>
-            <span>Pneu ${i}</span>
+            <span>${item.name}</span>
             <span class="badge">PCR</span>
           </div>
         </td>
         <td class="quantity">
-          <input class="t-right" type="text" id="item-${i}" name="quantity" oninput="onChangeQuantity(${i})">
+          <input class="t-right" type="text" id="item-supplier-${item.id}" name="quantity" oninput="onChangeQuantity(${item.id}, 'item-supplier')" value="${item.quantity}">
         </td>
       `;
       tbody.appendChild(tr);
-    }
+    });
 
     div.innerHTML = `
         <div class="supplier-info">
           <span class="nome">LL</span>
           <div class="totalizers">
-            <span>2500</span>
+            <span>${supplier.totalQuantity}</span>
             <span><i class="bi bi-box-seam"></i> 2</span>
           </div>
         </div>
@@ -108,15 +117,31 @@ function renderSupplierCart() {
   }
 }
 
-function onChangeQuantity(i) {
-  const item = document.getElementById(`item-${i}`);
-  let value = item.value.replace(/[^0-9]/g, "") ?? 0;
-  value = Number(value);
+function onChangeQuantity(itemId, inputName) {
+  let quantity = formatQuantity(itemId, inputName);
+  if (inputName === 'item') {
+    addProductToCart(itemId, quantity);
+  } else if (inputName === 'item-supplier') {
+    updateProduct(itemId, quantity);
+  }
+}
+
+function formatQuantity(itemId, inputName) {
+  const item = document.getElementById(`${inputName}-${itemId}`);
+  let value = item.value.replace(/[^0-9]/g, "") ?? '';
+  
+  if (inputName === 'item-supplier') {
+    value = Number(value || 1);
+  } else {
+    value = Number(value || 0);
+  }
+  
   item.value = formatNumber(value);
+  return value;
 }
 
 function formatNumber(value) {
-  return new Intl.NumberFormat('en-US').format(value);
+  return value ? new Intl.NumberFormat('en-US').format(value) : '';
 }
 
 function formatCurrency(value, locale = 'en-US', currency = 'USD') {
@@ -126,5 +151,54 @@ function formatCurrency(value, locale = 'en-US', currency = 'USD') {
   }).format(value);
 }
 
+let cart = [];
+
+function addProductToCart(itemId, quantity) {
+  if(!quantity) {
+    removeProduct(itemId);
+    return;
+  }
+  
+  const product = products.find(item => item.id == itemId);
+  if (!product) return;
+
+  const existingItem = cart.find(item => item.id === product.id);
+  if (existingItem) {
+    existingItem.quantity = quantity;
+    existingItem.total = existingItem.price * existingItem.quantity;
+    renderSupplierCart();
+    return;
+  }
+
+  const cartItem = {
+    id: product.id,
+    name: product.name,
+    quantity: quantity,
+    price: product.price,
+    total: product.price * quantity,
+    supplier: product.supplier
+  };
+
+  cart.push(cartItem);
+  renderSupplierCart();
+}
+
+function removeProduct(productId) {
+  cart = cart.filter(item => item.id != productId);
+  renderSupplierCart();
+}
+
+function updateProduct(productId, quantity) {
+  const item = products.find(item => item.id == productId);
+  if (item) {
+    item.quantity = quantity;
+  }
+
+  const input = document.getElementById(`item-${productId}`);
+  console.log(input);
+  if (input) {
+    input.value = quantity ? formatNumber(quantity) : '';
+  }
+}
 
 init();
